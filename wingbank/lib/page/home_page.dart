62b43b956f4fd13fd5ad47898cec58f54b33ff19
vengang <cheavengang123@gmail.com>
2026-04-03 14:44:45 +0700

    new update on homepage and img

--- wingbank/lib/page/home_page.dart
+++ wingbank/lib/page/home_page.dart
@@ -22,121 +22,184 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
+      appBar: _buildAppBar,
       drawer: _buildDrawer(),
-      body: Column(children: [_buildHeader(), _buildService()]),
+      // body: Column(children: [_buildHeader(), _buildService()]),
     );
   }
 
-  Widget _buildHeader() {
-    return Container(
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [primaryGreen, darkGreen],
-        ),
+  AppBar get _buildAppBar {
+    return AppBar(
+      backgroundColor: Color(0xFFa9cb39),
+      title: Image.asset(
+        'lib/images/wing.png',
+        width: 130,
+        height: 200,
+        fit: BoxFit.cover,
       ),
-      child: Padding(
-        padding: const EdgeInsetsDirectional.symmetric(
-          horizontal: 5.0,
-          vertical: 12.0,
-        ),
-        child: SafeArea(
-          bottom: false,
-          child: Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // menu icon
-              GestureDetector(
-                onTap: () {
-                  _scaffoldKey.currentState?.openDrawer();
-                },
-                child: const Icon(Icons.menu, color: Colors.white, size: 30.0),
-              ),
-              // wing logo
-              Container(
-                // padding: EdgeInsets.only(right: 30.0),
-                margin: EdgeInsets.only(right: 0.0),
-                child: Image.asset(
-                  'lib/images/wing.png',
-                  width: 200,
-                  height: 50,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 70.0),
-                // margin: EdgeInsets.only(right: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    // khmerQr
-                    GestureDetector(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('khmerQr Clicked'),
-                              content: const Text('khmerQr was clicked'),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('OK'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      child: Image.asset(
-                        'lib/images/khmerqr.png',
-                        width: 40,
-                        height: 50,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    // Rinng
-                    SizedBox(width: 8),
-                    GestureDetector(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Ring Clicked'),
-                              content: const Text('Ring was clicked'),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('OK'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      child: Image.asset(
-                        'lib/images/ringwing.png',
-                        width: 30,
-                        height: 40,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+      actions: [
+        // wingpoint
+        GestureDetector(
+          child: Image.asset(
+            'lib/images/wingpoint.png',
+            width: 40,
+            height: 90,
+            fit: BoxFit.cover,
           ),
         ),
-      ),
+        SizedBox(width: 10),
+        // khmerQr
+        GestureDetector(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('khmerQr Clicked'),
+                  content: const Text('khmerQr was clicked'),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('OK'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: Image.asset(
+            'lib/images/khqr.png',
+            width: 40,
+            height: 90,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(width: 10),
+        GestureDetector(
+          child: Image.asset(
+            'lib/images/ringwing.png',
+            width: 30,
+            height: 80,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(width: 10),
+      ],
     );
   }
+
+  // Widget _buildHeader() {
+  //   return Container(
+  //     width: double.infinity,
+  //     decoration: const BoxDecoration(
+  //       gradient: LinearGradient(
+  //         begin: Alignment.topLeft,
+  //         end: Alignment.bottomRight,
+  //         colors: [primaryGreen, darkGreen],
+  //       ),
+  //     ),
+  //     child: Padding(
+  //       padding: const EdgeInsetsDirectional.symmetric(
+  //         horizontal: 5.0,
+  //         vertical: 12.0,
+  //       ),
+  //       child: SafeArea(
+  //         bottom: false,
+  //         child: Row(
+  //           // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //           children: [
+  //             // menu icon
+  //             GestureDetector(
+  //               onTap: () {
+  //                 _scaffoldKey.currentState?.openDrawer();
+  //               },
+  //               child: const Icon(Icons.menu, color: Colors.white, size: 30.0),
+  //             ),
+  //             // wing logo
+  //             Container(
+  //               // padding: EdgeInsets.only(right: 30.0),
+  //               margin: EdgeInsets.only(right: 0.0),
+  //               child: Image.asset(
+  //                 'lib/images/wing.png',
+  //                 width: 200,
+  //                 height: 50,
+  //                 fit: BoxFit.cover,
+  //               ),
+  //             ),
+  //             Container(
+  //               padding: EdgeInsets.only(left: 70.0),
+  //               // margin: EdgeInsets.only(right: 20.0),
+  //               child: Row(
+  //                 mainAxisAlignment: MainAxisAlignment.end,
+  //                 children: [
+  //                   // khmerQr
+  //                   GestureDetector(
+  //                     onTap: () {
+  //                       showDialog(
+  //                         context: context,
+  //                         builder: (BuildContext context) {
+  //                           return AlertDialog(
+  //                             title: const Text('khmerQr Clicked'),
+  //                             content: const Text('khmerQr was clicked'),
+  //                             actions: <Widget>[
+  //                               TextButton(
+  //                                 child: const Text('OK'),
+  //                                 onPressed: () {
+  //                                   Navigator.of(context).pop();
+  //                                 },
+  //                               ),
+  //                             ],
+  //                           );
+  //                         },
+  //                       );
+  //                     },
+  //                     child: Image.asset(
+  //                       'lib/images/khmerqr.png',
+  //                       width: 40,
+  //                       height: 50,
+  //                       fit: BoxFit.cover,
+  //                     ),
+  //                   ),
+  //                   // Rinng
+  //                   SizedBox(width: 8),
+  //                   GestureDetector(
+  //                     onTap: () {
+  //                       showDialog(
+  //                         context: context,
+  //                         builder: (BuildContext context) {
+  //                           return AlertDialog(
+  //                             title: const Text('Ring Clicked'),
+  //                             content: const Text('Ring was clicked'),
+  //                             actions: <Widget>[
+  //                               TextButton(
+  //                                 child: const Text('OK'),
+  //                                 onPressed: () {
+  //                                   Navigator.of(context).pop();
+  //                                 },
+  //                               ),
+  //                             ],
+  //                           );
+  //                         },
+  //                       );
+  //                     },
+  //                     child: Image.asset(
+  //                       'lib/images/ringwing.png',
+  //                       width: 30,
+  //                       height: 40,
+  //                       fit: BoxFit.cover,
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 
   Widget _buildDrawer() {
     return Drawer();
