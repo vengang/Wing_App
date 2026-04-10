a2cb374c05f401059a5971e4f6ca63658bd1e244
kimsourkpc095-prog <kimsourkpc095@gmail.com>
2026-04-11 01:22:09 +0700

    11/4 update icon back

--- lib/widget/aboutPage.dart
+++ lib/widget/aboutPage.dart
@@ -11,7 +11,12 @@ class Aboutpage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 119, 189, 44),
-        leading: Icon(Icons.arrow_back_ios, color: AppColors.bgGrey),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios, color: AppColors.bgGrey),
+        ),
         title: Text("About", style: TextStyle(color: AppColors.bgGrey)),
         centerTitle: true,
       ),
