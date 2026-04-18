3d37d965cde97b458d04da78383653c5eb6160ad
vengang <cheavengang123@gmail.com>
2026-04-18 19:46:35 +0700

    18/4(3) nagivator action

--- lib/page/myApp.dart
+++ lib/page/myApp.dart
@@ -42,10 +42,10 @@ class _RightToLeftPageTransition extends PageTransitionsBuilder {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    const begin = Offset(1.0, 0.0); // right
+    const begin = Offset(1.0, 0.0);
     const end = Offset.zero;
 
-    final tween = Tween(
+    final tween = Tween<Offset>(
       begin: begin,
       end: end,
     ).chain(CurveTween(curve: Curves.easeInOut));
