86404d5c36af81d55fcab6029c371a534558d34d
kimsourkpc095-prog <kimsourkpc095@gmail.com>
2026-04-23 11:25:10 +0700

    23/4

--- lib/page/splash _screen.dart
+++ lib/page/splash _screen.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => HomePage()),
+        MaterialPageRoute(builder: (context) => Login()),       
       );
     });
   }
